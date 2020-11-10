96395bfae233cbb7edf124651fe1a6096bba155d
akasakashota <shota.akasaka@plaid.co.jp>
2020-11-10 15:47:32 +0900

    Refactoring

--- src/main/kotlin/BurpExtender.kt
+++ src/main/kotlin/BurpExtender.kt
@@ -6,6 +6,7 @@ import java.awt.datatransfer.StringSelection
 import java.awt.event.MouseEvent
 import java.awt.event.MouseListener
 import java.io.PrintWriter
+import java.lang.Exception
 import java.lang.RuntimeException
 import javax.swing.JMenuItem
 import kotlin.collections.MutableList
@@ -13,7 +14,7 @@ import kotlin.collections.MutableList
 
 class BurpExtender : IBurpExtender, IContextMenuFactory {
     private lateinit var cb: IBurpExtenderCallbacks
-    private lateinit var helper: IExtensionHelpers
+    private lateinit var helpers: IExtensionHelpers
     private lateinit var stdout: PrintWriter
     private lateinit var stderr: PrintWriter
 
@@ -24,7 +25,7 @@ class BurpExtender : IBurpExtender, IContextMenuFactory {
     override fun registerExtenderCallbacks(callbacks: IBurpExtenderCallbacks?) {
         callbacks?.also {
             cb = it
-            helper = cb.helpers
+            helpers = cb.helpers
             stdout = PrintWriter(cb.stdout, true)
             stderr = PrintWriter(cb.stderr, true)
         } ?: throw RuntimeException("Runtime Exception at Registration")
@@ -38,11 +39,9 @@ class BurpExtender : IBurpExtender, IContextMenuFactory {
     override fun createMenuItems(invocation: IContextMenuInvocation?): MutableList<JMenuItem> {
         invocation?.let {
             val httpRequestResponseArray = it.selectedMessages
-            val messages = convert(httpRequestResponseArray)
-            out("Select ${httpRequestResponseArray.size} item${if (httpRequestResponseArray.size > 1) "s" else ""}")
-
+            val formattedItems = httpRequestResponseArray.map { i -> Formatting(i) }
             val item = JMenuItem("Copy as Custom Format")
-            item.addMouseListener(CopyListener(messages))
+            item.addMouseListener(CopyListener(formattedItems))
             return mutableListOf(item)
         }
 
@@ -59,41 +58,63 @@ class BurpExtender : IBurpExtender, IContextMenuFactory {
         val sb = StringBuilder()
 
         items.forEach { http ->
-            val req = helper.analyzeRequest(http)
-            val res = helper.analyzeResponse(http.response)
-
-            sb.append(
-                arrayOf(
-                    req.method,
-                    req.url.toString(),
-                    http.request.decodeToString(req.bodyOffset),
-                    http.response.decodeToString(res.bodyOffset)
-                ).joinToString("\t")
-            )
-            sb.append(System.lineSeparator())
         }
 
         return sb.toString()
     }
 
+    private inner class CopyListener(private val formattedItems: List<Formatting>) : MouseListener {
+        override fun mouseReleased(e: MouseEvent?) {
+            try {
+                val clip: Clipboard = Toolkit.getDefaultToolkit()!!.systemClipboard
+                val ss = StringSelection(formattedItems.joinToString("\n"))
+                clip.setContents(ss, ss)
+                out("Copied ${formattedItems.size} item(s).")
+            } catch (e: Exception) { err(e) }
+        }
+
+        override fun mouseClicked(e: MouseEvent?) {}
+        override fun mousePressed(e: MouseEvent?) {}
+        override fun mouseEntered(e: MouseEvent?) {}
+        override fun mouseExited(e: MouseEvent?) {}
+    }
+
+    private inner class Formatting(private val http: IHttpRequestResponse) {
+        private var method: String = ""
+        private var url: String = ""
+        private var requestBody: String = ""
+        private var responseBody: String = ""
+
+        init {
+            try {
+                val req = helpers.analyzeRequest(http)
+                val res = helpers.analyzeResponse(http.response)
+
+                method = req.method
+                url = req.url.toString()
+                requestBody = http.request.decodeToString(req.bodyOffset)
+                responseBody = http.response.decodeToString(res.bodyOffset)
+            } catch (e: Exception) { err(e) }
+        }
+
+        private fun parseRequestBody() {
+
+        }
+
+        private fun parseResponseBody() {
+
+        }
+
+        override fun toString(): String {
+            return arrayOf(method, url, requestBody, responseBody).joinToString("\t")
+        }
+    }
+
     private fun out(msg: String) {
         stdout.println("[$EXTENSION_NAME][Info]: $msg")
     }
-
-    private fun err(msg: String) {
-        stderr.println("[$EXTENSION_NAME][Error]: $msg")
+    private fun err(e: Exception) {
+        stderr.println("[$EXTENSION_NAME][Error]: ${e.message}")
+        e.printStackTrace(stderr)
     }
-}
-
-class CopyListener(private val str: String) : MouseListener {
-    override fun mouseReleased(e: MouseEvent?) {
-        val clip: Clipboard = Toolkit.getDefaultToolkit()!!.systemClipboard
-        val ss = StringSelection(str)
-        clip.setContents(ss, ss)
-    }
-
-    override fun mouseClicked(e: MouseEvent?) {}
-    override fun mousePressed(e: MouseEvent?) {}
-    override fun mouseEntered(e: MouseEvent?) {}
-    override fun mouseExited(e: MouseEvent?) {}
 }
